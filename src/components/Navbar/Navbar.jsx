f9aac64260d8c9e25186fceb2cc1456eb731095e
countaustin1990 <65646074+countaustin1990@users.noreply.github.com>
2025-01-20 07:45:13 +0100

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   }, [location]);
 
   return (
-    <nav className="bg-gray-400 shadow-lg px-6 py-6">
+    <nav className="bg-gray-100 shadow-lg px-6 py-6">
       <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
