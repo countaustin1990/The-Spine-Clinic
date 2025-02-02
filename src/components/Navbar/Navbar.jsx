4a7a862cbe142686ce06abe8ccfa3d4258f73f15
Nsikak Usanga <31377369+usanga02@users.noreply.github.com>
2025-02-02 10:19:42 -0800

    add Logout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -153,6 +153,13 @@ const Navbar = () => {
                 Doctor Appointment
               </button>
             </Link>
+
+            <button
+              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              onClick={logout}
+            >
+              Logout
+            </button>
           </div>
         </div>
       )}
