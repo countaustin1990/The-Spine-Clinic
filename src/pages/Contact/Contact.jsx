6ac97dd34344f0e6f587062e531fd8bc3815a0cf
countaustin1990 <65646074+countaustin1990@users.noreply.github.com>
2025-02-26 18:08:45 +0100

    Update Contact.jsx

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -7,6 +7,7 @@ import {
   FaInstagram,
   FaLinkedin,
   FaPhone,
+  FaTwitter
 } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { HiOutlineArrowSmLeft } from "react-icons/hi";
