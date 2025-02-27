e86ba9a01b922b149acede6cc0b7c8562c458e20
countaustin1990 <65646074+countaustin1990@users.noreply.github.com>
2025-02-28 00:02:17 +0100

    Update PartnersSponsor.jsx

--- src/components/partners/PartnersSponsor.jsx
+++ src/components/partners/PartnersSponsor.jsx
@@ -1,18 +1,16 @@
-import HtsLogo from "../../assets/Htslogo.png"; // Ensure this path is correct
+import HtsLogo from "../../assets/Htslogo.png";
 import Najoda from "../../assets/najoda.png";
 import PearlsLogo from "../../assets/PearlsLogo.png";
 import EverCare from "../../assets/EverCare.png";
 import BenqLogo from "../../assets/BenqLogo.png";
 
-
-
 const partners = [
-  { name: "HtsLogo", logo: HtsLogo }, // Use imported image
-  { name: "Najoda", logo: Najoda }, // Public folder image
-  { name: "PearlsLogo", logo: PearlsLogo },
+  { name: "HTS Logo", logo: HtsLogo },
+  { name: "Najoda", logo: Najoda },
+  { name: "Pearls Logo", logo: PearlsLogo },
   { name: "EverCare", logo: EverCare },
-  { name: "BenqLogo", logo: BenqLogo },
-]
+  { name: "BenQ Logo", logo: BenqLogo },
+];
 
 const PartnersSponsors = () => {
   return (
@@ -25,14 +23,14 @@ const PartnersSponsors = () => {
           We are proud to collaborate with these amazing organizations.
         </p>
 
-        {/* Centering images */}
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 container">
+        {/* Grid layout for logos */}
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
           {partners.map((partner, index) => (
             <div key={index} className="flex items-center justify-center h-24">
               <img
                 src={partner.logo}
                 alt={partner.name}
-                className="h-16 md:h-20 items-center lg:h-24 object-contain grayscale hover:grayscale-0 transition-all duration-300"
+                className="h-16 md:h-20 lg:h-24 object-contain grayscale hover:grayscale-0 transition-all duration-300"
               />
             </div>
           ))}
