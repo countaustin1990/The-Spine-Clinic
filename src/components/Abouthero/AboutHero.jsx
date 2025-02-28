c41bbc88c8ffff66f64d821e1677154611c4c794
countaustin1990 <65646074+countaustin1990@users.noreply.github.com>
2025-02-28 14:28:52 +0100

    Update AboutHero.jsx

--- src/components/Abouthero/AboutHero.jsx
+++ src/components/Abouthero/AboutHero.jsx
@@ -1,6 +1,6 @@
 import { FaClinicMedical, FaPhoneAlt } from 'react-icons/fa';
 import { motion } from 'framer-motion';
-import Logo from '../../assets/abouthero.webp'
+import Logo from '../../assets/abouthero.webp';
 import { Link } from 'react-router-dom';
 
 const AboutHero = () => {
@@ -14,20 +14,19 @@ const AboutHero = () => {
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
         >
-          <h1 className="text-4xl md:text-5xl font-bold text-blue-900 leading-tight mb-4 flex items-center">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-blue-900 leading-tight mb-4 flex items-center">
             <FaClinicMedical className="text-blue-700 mr-3" />
             Advanced Knee Care
           </h1>
-          <p className="text-lg text-blue-800 mb-6">
+          <p className="text-lg sm:text-xl text-blue-800 mb-6">
             At <span className="font-semibold">The Spinic</span>, we specialize in cutting-edge, minimally invasive knee care, ensuring faster recovery and better outcomes for our patients.
           </p>
           <div className="mt-4">
              <Link to="/blog">
-                <button className="bg-blue-700 text-white py-3 px-8 rounded-full shadow-lg hover:bg-blue-600 transition duration-300">
+                <button className="bg-blue-700 text-white py-3 px-8 rounded-full shadow-lg hover:bg-blue-600 focus:ring-2 focus:ring-blue-600 transition duration-300">
                     Learn More About Us 
                 </button>
              </Link>
-           
           </div>
         </motion.div>
 
@@ -48,14 +47,13 @@ const AboutHero = () => {
 
       {/* Contact Info */}
       <motion.div
-        className="absolute bottom-8 right-8 bg-white p-4 rounded-lg shadow-md flex items-center"
+        className="absolute bottom-8 right-8 bg-white p-4 rounded-lg shadow-md flex items-center space-x-2"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 1, duration: 1 }}
       >
-        <FaPhoneAlt className="text-blue-700 mr-3" />
-        
-        <span className="text-blue-800 font-medium">Call Us: (+234) 803 890 9678 or  (+234) 806-430-6055</span>
+        <FaPhoneAlt className="text-blue-700" />
+        <span className="text-blue-800 font-medium text-sm sm:text-base">Call Us: (+234) 803 890 9678 or (+234) 806-430-6055</span>
       </motion.div>
     </section>
   );
